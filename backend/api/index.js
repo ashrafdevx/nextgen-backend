6a508675c28279898e436876768f4d8a13bcb302
ashrafnasib <ashraf.devx@gmail.com>
2025-01-09 23:04:12 +0500

    Update index.js

--- backend/api/index.js
+++ backend/api/index.js
@@ -1,5 +1,6 @@
 import express from "express";
 import cors from "cors";
+const PORT = process.env.PORT || 3000; // Use Vercel's environment variable or default to 3000
 
 const app = express();
 
@@ -7,9 +8,12 @@ const app = express();
 app.use(express.json());
 app.use(cors());
 // Routes
-app.get("/", (req, res) => {
-  res.send("Hello from Express on Vercel!");
+app.get("/api", (req, res) => {
+  res.json({ message: "Hello from the backend API!" });
 });
 
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
 // Export the app
 export default app;
