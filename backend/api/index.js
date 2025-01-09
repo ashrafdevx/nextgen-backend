66231cfdb68f19bbf254d9aad9bbd9db20e0c0c2
ashrafnasib <ashraf.devx@gmail.com>
2025-01-09 22:15:38 +0500

    cors added

--- backend/api/index.js
+++ backend/api/index.js
@@ -1,9 +1,11 @@
 import express from "express";
+import cors from "cors";
+
 const app = express();
 
 // Middleware
 app.use(express.json());
-
+app.use(cors());
 // Routes
 app.get("/", (req, res) => {
   res.send("Hello from Express on Vercel!");
